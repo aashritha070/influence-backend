2f402441089e3920614085fd80c46c30443a0528
Ashwin <ashwins.2019a@vitstudent.ac.in>
2022-10-23 01:58:48 +0530

    updated api path

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,21 +1,21 @@
 const router = require('express').Router();
 const { createBlog, editBlogById, deleteBlogById, fetchTopBlog, fetchAllBlog, fetchBlogById, fetchBlogByAuthor, fetchBlogByTags } = require('../controllers/blogControllers');
 
-router.post('/', (req, res) => createBlog(req, res)); //create blog
+router.post('/create', (req, res) => createBlog(req, res)); //create blog
 
-router.put('/', (req, res) => editBlogById(req, res)); // update blog
+router.post('/edit', (req, res) => editBlogById(req, res)); // update blog
 
-router.delete("/", (req, res) => deleteBlogById(req, res)); // delete blog
+router.post("/delete", (req, res) => deleteBlogById(req, res)); // delete blog
 
-router.get("/id", (req, res) => fetchBlogById(req, res)); // fetch blog by id
+router.post("/id", (req, res) => fetchBlogById(req, res)); // fetch blog by id
 
-router.get("/tag", (req, res) => fetchBlogByTags(req, res)); // fetch all blog
+router.post("/tag", (req, res) => fetchBlogByTags(req, res)); // fetch all blog
 
-router.get("/author", (req, res) => fetchBlogByAuthor(req, res)); // fetch all blog
+router.post("/author", (req, res) => fetchBlogByAuthor(req, res)); // fetch all blog
 
-router.get("/", (req, res) => fetchTopBlog(req, res)); // fetch top blog
+router.post("/", (req, res) => fetchTopBlog(req, res)); // fetch top blog
 
-router.get("/all", (req, res) => fetchAllBlog(req, res)); // fetch all blog
+router.post("/all", (req, res) => fetchAllBlog(req, res)); // fetch all blog
 
 
 module.exports = router;
