29fbed9a26398eee5976c5235068bf581e4c7377
Ashwin <ashwins.2019a@vitstudent.ac.in>
2022-10-23 02:02:03 +0530

    tags api path updated

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-router.put('/:id', (req, res) => updateBlog(req, res));
+router.post('/edit', (req, res) => updateBlog(req, res));
 
-router.delete('/:id', (req, res) => deleteBlog(req, res));
+router.post('/delete', (req, res) => deleteBlog(req, res));
 
-router.get('/:id', (req, res) => fetchBlog(req, res));
+router.post('/', (req, res) => fetchBlog(req, res));
 
 module.exports = router;
--- routes/tagRoutes.js
+++ routes/tagRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { fetchAlltags } = require("../controllers/tagControllers")
 
-router.get("/", (req, res) => fetchAlltags(req, res)); // fetch all tag
+router.post("/", (req, res) => fetchAlltags(req, res)); // fetch all tag
 
 module.exports = router;
